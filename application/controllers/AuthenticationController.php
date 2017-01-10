1ab8d9b44cace5407cecff8092d88816d8a74c16
YCelli Mohanty <Celli.Mohanty@cognizant.com>
2017-01-10 15:55:50 +0530

    second commit

--- application/controllers/AuthenticationController.php
+++ application/controllers/AuthenticationController.php
@@ -7,7 +7,8 @@ class AuthenticationController extends Zend_Controller_Action
 	}
     public function loginAction()
     {
-	$this->view->hello="welcome Celli";
+	$this->view->hello="welcome dixita";
+	$this->view->hello="welcome dixita";
 	$this->view->title="Vendor Request System";
         }
 		}
