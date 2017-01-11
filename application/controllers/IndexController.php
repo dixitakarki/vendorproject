4bc528dd149c3f817b4537da68bc3d59f2d5d982
YCelli Mohanty <Celli.Mohanty@cognizant.com>
2017-01-11 16:14:26 +0530

    third commit

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,12 +5,15 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+        $this->initView();
+		$this->view->baseUrl=$this->_request->getBaseUrl();
     }
 
     public function indexAction()
     {
         // action body
+		
+		
     }
 
 
