2adf66860f36eaaee741fb121b61af17824c4cc1
YCelli Mohanty <Celli.Mohanty@cognizant.com>
2017-01-16 18:11:35 +0530

    checkbox query completed

--- public/js/myscript.js
+++ public/js/myscript.js
@@ -1,13 +1,7 @@
-
-$(document).ready(function(){
+$(function() {
     $('#login-form-link').click(function(e) {
     	$("#login-form").delay(100).fadeIn(100);
 		$(this).addClass('active');
 		e.preventDefault();
 	});
-<<<<<<< HEAD
-	
 });
-=======
-});
->>>>>>> 8fc751a8c9aef554088bfeebe3809d129cda92a6
